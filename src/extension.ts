9e7c3ef99fd8190d4494a3f101cf9c535e9283b8
Sparsh Agarwal <sparshaga25@gmail.com>
2021-08-05 13:16:57 +0530

    Implemented Basic Functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as configFilesData from './configFilesData.json';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "config-generator" is now active!'
-  );
+  let disposable = vscode.commands.registerCommand('config-generator.configGenerate', async () => {
+    const configFile = await vscode.window.showQuickPick(configFilesData, {
+      matchOnDetail: true,
+    });
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "config-generator.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from Config Generator!"
+    if (configFile) {
+      fs.readFile(
+        path.join(__dirname, '../src/data', configFile.fileName),
+        {
+          encoding: 'utf8',
+        },
+        (error, data) => {
+          error && console.log(error);
+
+          // TODO: Ask for correct workspaceFolder if multiple in the same workspace
+          if (vscode.workspace.workspaceFolders) {
+            fs.writeFileSync(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, configFile.fileName), data);
+          }
+        },
       );
     }
-  );
+  });
 
   context.subscriptions.push(disposable);
 }
